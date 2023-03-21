c258eccfb751aa967ba157a220ec976de5b01663
darya-startsava <daryastartsava@gmail.com>
2023-03-21 03:25:09 +0100

    feat: add boards list to the main page

---
+++ project-management-app/src/app/main-page/boards.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { endpoints } from '../constants';
+
+export interface GetAllBoardsInterface {
+  _id: string;
+  title: string;
+  owner: string;
+  users: Array<string>;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BoardsService {
+  url = endpoints.boards;
+  boards: Array<GetAllBoardsInterface> = [];
+
+  constructor(private http: HttpClient) {}
+
+  getAllBoards(token: string): Observable<Array<GetAllBoardsInterface>> {
+    return this.http
+      .get<any>(this.url, {
+        headers: new HttpHeaders({
+          Authorization: `${token}`,
+        }),
+      })
+      .pipe(
+        tap({
+          next: (response) => (this.boards = response),
+        })
+      );
+  }
+}
--- project-management-app/src/app/main-page/main-page.component.ts
+++ project-management-app/src/app/main-page/main-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs';
+import { SignInService } from '../sign-in-page/sign-in.service';
+import { BoardsService, GetAllBoardsInterface } from './boards.service';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
+  token = '';
+  boards: Array<GetAllBoardsInterface> = [];
 
+  constructor(
+    public signInService: SignInService,
+    private boardsService: BoardsService
+  ) {}
+
+  ngOnInit() {
+    this.token = this.signInService.token;
+    this.boardsService
+      .getAllBoards(this.token)
+      .pipe(take(1))
+      .subscribe({ next: (boards) => (this.boards = boards) });
+  }
+  
+  onOpenConfirmation() {}
 }
